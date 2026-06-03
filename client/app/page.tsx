98adefe00a5b3075a62ad67e4e1a881f865211eb
Nirav Balvantrai Prajapati <NPrajapati@a10networks.com>
2026-06-03 21:57:46 +0530

    Remove client submodule

---
+++ client/app/page.tsx
@@ -0,0 +1,16 @@
+import FileUploadComponent from "./components/file-upload";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="min-h-screen w-screen flex">
+        <div className="w-[30vw] min-h-screen p-4 flex justify-center items-center">
+          <FileUploadComponent />
+        </div>
+        <div className="w-[70vw] min-h-screen border-l-2">
+          Chat Component
+        </div>
+      </div>
+    </div>
+  );
+}
