065ae0cd09e26c4e22aa56aab54bb255f0b1beb4
Nirav Balvantrai Prajapati <NPrajapati@a10networks.com>
2026-06-04 08:33:52 +0530

    node server index file chnages

--- server/index.js
+++ server/index.js
@@ -6,35 +6,45 @@ import { OpenAIEmbeddings } from "@langchain/openai";
 import { QdrantVectorStore } from "@langchain/qdrant";
 import OpenAI from "openai";
 
+const client = new OpenAI({
+  apiKey: "",
+});
+
+const queue = new Queue("file-upload-queue", {
+  connection: {
+    host: "localhost",
+    port: "6379",
+  },
+});
+
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb){
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        cb(null, `${uniqueSuffix}-${file.originalname}`);
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  },
+});
 const upload = multer({ storage: storage });
 
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  return res.json({ status: "All Good" });
+  return res.json({ status: "Root Route to test server is up and running!" });
 });
 
-app.post('/upload/pdf', upload.single('pdf'), async (req, res) => {
-    await queue.add(
-        'file-ready',
-        JSON.stringify({
-            filename: req.file.originalname,
-            destination: req.file.destination,
-            path: req.file.path
-        })
-    )
-    return res.json({message: 'uploaded'})
-})
+app.post("/upload/pdf", upload.single("pdf"), async (req, res) => {
+  await queue.add(
+    "file-ready",
+    JSON.stringify({
+      filename: req.file.originalname,
+      destination: req.file.destination,
+      path: req.file.path,
+    }),
+  );
+  return res.json({ message: "uploaded" });
+});
 
-
-app.listen(8000, () => console.log(`Server started on PORT: ${8000}`))
+app.listen(8000, () => console.log(`Server started on PORT: ${8000}`));
